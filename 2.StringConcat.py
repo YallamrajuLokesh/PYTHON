21061f60dc1392e1f7a8ab17574f9edc40d6f71e
YallamrajuLokesh <149151515+YallamrajuLokesh@users.noreply.github.com>
2023-12-08 18:13:46 +0530

    Create 2.StringConcat.py

---
+++ 2.StringConcat.py
@@ -0,0 +1,12 @@
+print("hello world")
+print("hello world")
+print("hello world")
+
+//above code can be simplified to
+print("hello world\nhello world\nhello world\n")
+
+print("loki"+"god")
+//give space
+print("loki"+" "+"god")
+//orr
+print("loki "+"god")
