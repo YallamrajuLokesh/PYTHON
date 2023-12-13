41c3b090b2b2d54e565f495edd988d5833e2e3b0
YallamrajuLokesh <149151515+YallamrajuLokesh@users.noreply.github.com>
2023-12-13 13:37:09 +0530

    Update 7.Function.py

--- 7.Function.py
+++ 7.Function.py
@@ -1,6 +1,22 @@
 def my_function():
   print("here is my first function")
 
+
+
+def greet():
+  print("hello morning")
+  print("today is Wednesday ")
+  print("today's weather is great ")
+
+#using argument and parameters 
+
+def greet_with_name(name):
+  print(f"hi {name}")
+  print(f"how are you doing today {name}")
+
+greet_with_name("loki")
+
+
 my_function() #calling function
 
 #while loops
